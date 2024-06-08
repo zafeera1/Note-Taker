dd43fed2a9fb78ada4246fa24acffe1af694b91b
unknown <rnaz.alluri@gmail.com>
2024-06-08 12:34:13 -0700

    Added routes file

---
+++ Develop/public/assets/js/routes.js
@@ -0,0 +1,75 @@
+// routes.js
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const router = express.Router();
+
+const dbPath = path.join(__dirname, '../../../db/db.json');
+
+// GET all notes
+router.get('/notes', (req, res) => {
+  fs.readFile(dbPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+// POST a new note
+router.post('/notes', (req, res) => {
+  const newNote = req.body;
+  fs.readFile(dbPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    let notes = JSON.parse(data);
+    newNote.id = generateUniqueId(notes);
+    notes.push(newNote);
+    fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+      res.json(newNote);
+    });
+  });
+});
+
+// DELETE a note
+router.delete('/notes/:id', (req, res) => {
+  const idToDelete = parseInt(req.params.id);
+  fs.readFile(dbPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.id !== idToDelete);
+    fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+      res.json({ message: 'Note deleted successfully' });
+    });
+  });
+});
+
+function generateUniqueId(notes) {
+  let maxId = 0;
+  for (const note of notes) {
+    maxId = Math.max(maxId, note.id);
+  }
+  return maxId + 1;
+}
+
+module.exports = router;
---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const routes = require('./public/assets/js');
+const index = require('./public/assets/js/index')
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const dbPath = path.join(__dirname, 'db', 'db.json');
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Attach routes
+app.use(routes);
+app.use(index);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
